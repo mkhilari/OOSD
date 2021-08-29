9f1675d1d1ccce3609fcba085c9bc2fbbd04ab06
Manish Khilari <mkhilari@Manishs-MacBook-Air.local>
2021-08-29 15:53:39 +1000

    Comparable and sortable

--- Enrollable/Main.java
+++ Enrollable/Main.java
@@ -1,8 +1,12 @@
-
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        Subject aSubject = new Subject(20003, "OOSD");
+
+        // Sort array of students 
+        Arrays.sort(aSubject.getStudents().toArray());
     }
 }
